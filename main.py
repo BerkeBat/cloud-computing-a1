31a49fa47b9627b7cf0d0570a5c5bfc6b6217cb9
Berke <s3782067@student.rmit.edu.au>
2021-03-20 01:29:06 +1100

    user page made, login/register have forms

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+from google.auth.transport import requests
 from google.cloud import datastore
+import google.oauth2.id_token
 
 datastore_client = datastore.Client()
 app = Flask(__name__)
@@ -9,14 +11,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
+
+@app.route('/user/<string:username>', methods=['GET', 'POST'])
+def user(username):
+    # query = datastore_client.query(kind="user")
+    # query.add_filter("user_name", "=", username)
+    # fetched_username = query.fetch()
+    return render_template('user.html')
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
