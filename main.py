0488229b04b6398f941071e6b7a50d43ab39d258
Berke <s3782067@student.rmit.edu.au>
2021-03-22 00:27:46 +1100

    register checks for existing id/gives alert. user page change pass words, gives alert.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from urllib.parse import uses_fragment
 from flask import Flask, json, render_template, request, redirect
+from flask.globals import current_app
 from flask.helpers import url_for
 from google.auth.transport import requests
 from google.cloud import datastore, storage
@@ -12,6 +13,7 @@ logging.basicConfig(level=logging.DEBUG)
 datastore_client = datastore.Client()
 storage_client = storage.Client()
 app = Flask(__name__)
+current_user = "none"
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -21,6 +23,8 @@ def index():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    # if request.method == 'POST':
+    #     current_user = 
     return render_template('login.html')
 
 
@@ -30,24 +34,27 @@ def register():
         kind = "user"
         id = request.form["userid"]
         user_key = datastore_client.key(kind, id)
-        # if get_user_by_userid(user_key) is None:
-        newUser = datastore.Entity(key=user_key)
-        username = request.form['username']
-        password = request.form['pass']
-        # user_image = request.form['userimage']
-        newUser["user_name"] = username
-        newUser["password"] = password
-        # upload_userimage(user_image, username)
-        datastore_client.put(newUser)
-        # else:
-            # app.logger.info("user already exists.")
+        if get_user_by_userid(id) == None:
+            newUser = datastore.Entity(key=user_key)
+            username = request.form['username']
+            password = request.form['pass']
+            # user_image = request.form['userimage']
+            newUser["user_name"] = username
+            newUser["password"] = password
+            # upload_userimage(user_image, username)
+            datastore_client.put(newUser)
 
-        return redirect(url_for('login'))
+
+            return redirect(url_for('login'))
+        else:
+            return render_template('register.html', showalert=True)
+
     else:
-        return render_template('register.html')
+        return render_template('register.html', showalert=False)
 
 @app.route('/user/<string:userid>', methods=['POST', 'GET'])
 def user(userid):
+    pass_change_valid = True
     if request.method == 'POST':
         with datastore_client.transaction():
             user_key = datastore_client.key("user", userid)
@@ -55,12 +62,20 @@ def user(userid):
             if(user_to_change_pass_of['password'] == request.form['currpass']):
                 user_to_change_pass_of["password"] = request.form['newpass']
                 datastore_client.put(user_to_change_pass_of)
+                pass_change_valid = True
+            else:
+                pass_change_valid = False
+
     user = get_user_by_userid(userid)
-    return render_template('user.html', user = user)
+    if user == None:
+        return "User does not exist"
+    else:
+        return render_template('user.html', user = user, pass_change_valid=pass_change_valid)
 
 def get_user_by_userid(userid):
-    user_key = datastore_client.key("user", userid)
+    user_key = datastore_client.key("user", str(userid))
     gotten_user = datastore_client.get(user_key)
+    app.logger.info(gotten_user)
     return  gotten_user
     
 def upload_userimage(selected_image, image_name):
